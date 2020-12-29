5fcde2fe16ccf5064fe93c6c566d36821bb76980
ngochoang010900 <ngochoang0109giolinh@gmail.com>
2020-12-29 15:31:50 +0700

    3333 Signed-off-by: ngochoang010900 <ngochoang0109giolinh@gmail.com>

---
+++ FlightTicket/FlightTicket/Areas/Employee/EmployeeQuery/HomeQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FlightTicket.Areas.Employee.EmployeeQuery
+{
+    public class HomeQuery
+    {
+        //public static bool LoginEmployee()
+        //{
+
+        //}
+    }
+}
