e263f080c40ed7fc33e03d3fcf3d76b3612c48bf
ngochoang010900 <ngochoang0109giolinh@gmail.com>
2021-01-13 18:30:06 +0700

    333 Signed-off-by: ngochoang010900 <ngochoang0109giolinh@gmail.com>

---
+++ FlightTicket/FlightTicket/Controllers/BookTicket01Controller.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FlightTicket.Models;
+using System.Net.Mail;
+using System.Net;
+
+namespace FlightTicket.Controllers
+{
+    public class BookTicket01Controller : Controller
+    {
+        // GET: BookTicket01
+        // GET: BookTicket
+        private DB_A6C0B2_Nhom13FlightTicketEntities db = new DB_A6C0B2_Nhom13FlightTicketEntities();
+        public ActionResult Index()
+        {
+            ViewBag.lstCbay = db.ChuyenBays.Include("SanBay").ToList();
+            ViewBag.lstHangVe = new SelectList(db.HangVes, "MaHangVe", "TenHangVe");
+            return View();
+        }
+        public ActionResult SelectFlight(string idCB, string hangVe)
+        {
+            NguoiDung u = Session["nguoiDung"] as NguoiDung;
+            if(u == null)
+            {
+                ViewBag.mess = "Ban phai dang nhap de dat ve";
+                return Content("Error");
+            }
+
+            int id = Convert.ToInt32(idCB);
+            ChuyenBay cb = db.ChuyenBays.Where(x => x.MaCB == id).FirstOrDefault();
+            TimeSpan time = DateTime.Now - cb.NgayGioKhoiHanh;
+            if (time.Days > cb.NgayDatChamNhat)
+            {
+                return Content("Tre han");
+            }
+            Session["chuyenBay"] = cb;
+            Session["hangVe"] = hangVe;
+            return RedirectToAction("Form", "BookTicket01");
+        }
+        public ActionResult Form()
+        {
+            NguoiDung u = Session["nguoiDung"] as NguoiDung;
+            if (u == null)
+            {
+                ViewBag.mess = "Ban phai dang nhap de dat ve";
+                return View("Error");
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GetUserInfo(NguoiDung user)
+        {
+            Session["nguoiDung"] = user;
+            return RedirectToAction("ShowSeats");
+        }
+        public ActionResult ShowSeats()
+        {
+            var cbay = Session["chuyenBay"] as ChuyenBay;
+            int hangVe = Convert.ToInt32(Session["hangVe"]);
+
+            int idCB = cbay.MaCB;
+            var lstChoNgoi = db.ChoNgois.Where(x => x.MaCB == idCB && x.MaHangVe == hangVe).ToList();
+            return View();
+
+        }
+        public ActionResult getData()
+        {
+            var lstChoNgoi = db.ChoNgois.ToList();
+            var myList = (from c in lstChoNgoi
+                          select new { c.MaGhe, c.SoGhe, c.TinhTrang }).ToList();
+            return Json(myList, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult SelectSeat(string MaGhe)
+        {
+            Session["MaGhe"] = MaGhe;
+            int id = Convert.ToInt32(MaGhe);
+            using (var context = new DB_A6C0B2_Nhom13FlightTicketEntities())
+            {
+                var ghe = context.ChoNgois.Where(x => x.MaGhe == id).FirstOrDefault();
+                ghe.TinhTrang = false;
+                context.SaveChanges();
+            }
+            return Content("");
+        }
+        private void sendEmail(string mail)
+        {
+            //email của dự án
+            string email = "nhomltweb@gmail.com";
+            string password = "123456789a@";
+
+
+
+            var loginInfo = new NetworkCredential(email, password);
+            var msg = new MailMessage();
+            var smtpClient = new SmtpClient("smtp.gmail.com", 25);
+
+
+            msg.From = new MailAddress(email);
+            msg.To.Add(mail);
+            msg.Subject = "Hello";
+            msg.Body = "Ban da dat ve may bay thanh cong";
+            msg.IsBodyHtml = true;
+
+            //gán cho biến TempData để có thể kiểm tra xem user nhập đúng không 
+
+
+            smtpClient.EnableSsl = true;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = loginInfo;
+            smtpClient.Send(msg);
+
+
+        }
+        public ActionResult PaySuccess()
+        {
+            ////luu thong tin ve
+            var cbay = Session["chuyenBay"] as ChuyenBay;
+            var hangve = Convert.ToInt32(Session["hangVe"]);
+            var user = Session["nguoiDung"] as NguoiDung;
+            var maGhe = Convert.ToInt32(Session["MaGhe"]);
+            ////gui mail thong bao
+            sendEmail(user.Gmail);
+            using (var context = new DB_A6C0B2_Nhom13FlightTicketEntities())
+            {
+                VeChuyenBay ve = new VeChuyenBay() { MaNguoiDung = user.MaNguoiDung, NgayDat = DateTime.Now, MaGhe = maGhe };
+                context.VeChuyenBays.Add(ve);
+                context.SaveChanges();
+            }
+            ViewBag.cbay = cbay;
+            ViewBag.hangve = db.HangVes.Where(x => x.MaHangVe == hangve).FirstOrDefault();
+            ViewBag.Ghe = db.ChoNgois.Where(x => x.MaGhe == maGhe).FirstOrDefault();
+            return View();
+        }
+    }
+}
---
+++ FlightTicket/FlightTicket/Controllers/Homepage01Controller.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FlightTicket.Models;
+
+namespace FlightTicket.Controllers
+{
+    public class Homepage01Controller : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult LoginCheck(FormCollection form)
+        {
+            string tk = form["account"];
+            string pass = Encrypt.GetMD5(form["password"]);
+            using (var db = new DB_A6C0B2_Nhom13FlightTicketEntities())
+            {
+                var user = db.NguoiDungs.Where(x => x.TaiKhoan == tk && x.MatKhau == pass).FirstOrDefault();
+                if (user != null)
+                {
+                    Session["nguoiDung"] = user;
+                    return Redirect("/BookTicket01/Index");
+                }
+                else
+                {
+                    ViewBag.mess = "Dang nhap that bai";
+                    return View("Error");
+                }
+            }
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult RegisterCheck(FormCollection form)
+        {
+            string tk = form["account"];
+            string pass = Encrypt.GetMD5(form["password"]);
+            NguoiDung u = new NguoiDung() { TaiKhoan = tk, MatKhau = pass, CMND="",Gmail="",DiaChi="",ID_Quyen=3,SoDT="",NgaySinh=DateTime.Now,HoTen="" };
+            using (var db = new DB_A6C0B2_Nhom13FlightTicketEntities())
+            {
+                var user = db.NguoiDungs.Where(x => x.TaiKhoan == tk).FirstOrDefault();
+                if (user == null)
+                {
+                    using(var context=new DB_A6C0B2_Nhom13FlightTicketEntities())
+                    {
+                        context.NguoiDungs.Add(u);
+                        context.SaveChanges();
+                    }
+                    
+                    Session["nguoiDung"] = user;
+                    return Redirect("/BookTicket01/Index");
+                }
+                else
+                {
+                    ViewBag.mess = "Dang ky that bai";
+                    return View("Error");
+                }
+            }
+        }
+    }
+}
